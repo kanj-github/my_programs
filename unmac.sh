08a1c9c9f3439af1c2d1167abd95203ffa06b5ff
unknown <murderthemuggles@yahoo.co.in>
2018-01-05 21:29:12 +0530

    Script to remove mac turds

---
+++ unmac.sh
@@ -0,0 +1,15 @@
+if [ $# -eq 1 ]; then
+	echo Unmacing $1
+	find $1 -name "\._*" -delete
+	cd $1
+	if [ -d .Spotlight-V100 ]; then
+		rm -fr .Spotlight-V100
+	fi
+	if [ -d .Trashes ]; then
+		rm -fr .Trashes
+	fi
+	if [ -d .fseventsd ]; then
+		rm -fr .fseventsd
+	fi
+	cd -
+fi
