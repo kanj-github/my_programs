43bfa7c47a66a850a2fce9530ebc1909fef07eeb
U-GREEN\kanj <murderthemuggles@yahoo.co.in>
2018-06-16 20:10:43 +0530

    Fix case of macturd DS_Store

--- unmac.sh
+++ unmac.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 1 ]; then
 	echo Unmacing $1
-	find $1 \( -name "\._*" -o -name ".DS_store" \) -delete
+	find $1 \( -name "\._*" -o -name ".DS_Store" \) -delete
 	cd $1
 	if [ -d .Spotlight-V100 ]; then
 		rm -fr .Spotlight-V100
